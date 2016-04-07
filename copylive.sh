6e500ede7ba8eef77b5106c43fafeba9883771fe
gfcapalbo <giovanni@therp.nl>
2016-04-07 10:57:56 +0200

    improvements

--- copylive.sh
+++ copylive.sh
@@ -8,6 +8,12 @@ cd /tmp
 function help {
     echo"
     if the username and the database name are the same:
+    Options:
+
+    -s   SHH Host to fetch live user
+    -m   PERFORM MAGENTO OPERATIONS
+
+    MINIMAL USAGE:
 
     copylive  source_user  target_user
 
@@ -22,48 +28,48 @@ function help {
 
     Magento extension:
 
-    if the -m switch is specified it will clean up all magneto connector related connections to avoid TARGET from connecting to the 
+    if the -m switch is specified it will clean up all magneto connector related connections to avoid TARGET from connecting to the production magento." 
 
-    Before doing that it will fetch the old target options and save them in 
-    ~home/magento_options.txt"
 }    
 
 function magento {
     echo " magento cleanup functionality still not implemented"
 }
 
-
 function noarguments {
     if [ -z "$1" ] || [ -z  "$2"]; then
     help
-    echo " ABORTING , missing minimal arguments"
-    }
+    echo "ABORTING , missing minimal arguments"
+}
  
-LIVE_USER=$1
+ORIGIN_USER=$1
 TARGET_USER=$2
 
 function no_livedb {
     if [-z "$4" ]; then
         if [-z "$3" ]; then
-            LIVE_DB=$1
+            ORIGIN_DB=$1
             TARGET_DB=$2
-            echo "USING DEFAULT NAME FOR SOURCE DATABASE: ${LIVE_DB}"
+            echo "USING DEFAULT NAME FOR SOURCE DATABASE: ${ORIGIN_DB}"
             echo "USING DEFAULT NAME FOR TARGET DATABASE: ${TARGET_DB}"
         else    
-            LIVE_DB=$3
-            TARGEddT_DB=$2
-            echo "USING Specified name for SOURCE DATABASE: ${LIVE_DB} "
+            ORIGIN_DB=$3
+            TARGET_DB=$2
+            echo "USING Specified name for SOURCE DATABASE: ${ORIGIN_DB} "
             echo "USING DEFAULT NAME FOR TARGET DATABASE: ${TARGET_DB}" 
         fi
     else
-        LIVE_DB=$3
+        ORIGIN_DB=$3
         TARGET_DB=$4
     fi
 }
 
-
-while getopts ":h:m" opt; do
+while getopts ":h:m:s" opt; do
     case $opt in
+    s)
+        ORIGIN_HOST=$OPTARG
+        SSH="ssh $ORIGIN_USER@$ORIGIN_HOST"
+        echo "SSH Live user set on ${SSH}"
     h)
         help
         exit
@@ -79,20 +85,20 @@ while getopts ":h:m" opt; do
 done
 
 
-TEST_USER="openerp"
-DBNAME="${LIVE_DB}_$(date +%F)"
-LIVE_HOST=192.168.11.31
-SSH="ssh $LIVE_USER@$LIVE_HOST"
+#TARGET_USER="openerp"
+#DBNAME="${ORIGIN_DB}_$(date +%F)"  #using default name
+DBNAME=${TARGET_DB}
 
-sudo -u $TEST_USER sh -c "cd ~$TEST_USER/data/filestore; ln -s attachments $DBNAME || true"
-sudo -u $TEST_USER rsync --recursive -q $LIVE_USER@$LIVE_HOST:data/filestore/$LIVE_DB/ /home/$TEST_USER/data/filestore/attachments/ &
-sudo -u $TEST_USER dropdb ${DBNAME} || true
-sudo -u $TEST_USER createdb ${DBNAME}
-$SSH "pg_dump --format=custom --no-owner $LIVE_DB | gzip --stdout" | gzip --decompress --stdout | sudo -u $TEST_USER pg_restore --no-owner --dbname=${DBNAME}
-sudo -u $TEST_USER psql $DBNAME -c "delete from ir_mail_server"
-sudo -u $TEST_USER psql $DBNAME -c "update fetchmail_server set active=False"
-sudo -u $TEST_USER psql $DBNAME -c "update ir_cron set active=False"
-sudo -u $TEST_USER psql $DBNAME -c "update ir_config_parameter set value='http://192.168.11.233' where key='web.base.url'"
-sudo -u $TEST_USER psql $DBNAME -c "update ir_module_module set state='uninstalled' where name in ('dead_mans_switch_client', 'dead_mans_switch_therp')"
-sudo -u $TEST_USER psql $DBNAME -c "update res_users set password_crypt=(select password_crypt from res_users where login='admin')"
+
+#sudo -u $TARGET_USER sh -c "cd ~$TARGET_USER/data/filestore; ln -s attachments $DBNAME || true"
+#sudo -u $TARGET_USER rsync --recursive -q $ORIGIN_USER@$ORIGIN_HOST:data/filestore/$ORIGIN_DB/ /home/$TARGET_USER/data/filestore/attachments/ &
+#sudo -u $TARGET_USER dropdb ${DBNAME} || true
+#sudo -u $TARGET_USER createdb ${DBNAME}
+#$SSH "pg_dump --format=custom --no-owner $ORIGIN_DB | gzip --stdout" | gzip --decompress --stdout | sudo -u $TARGET_USER pg_restore --no-owner --dbname=${DBNAME}
+#sudo -u $TARGET_USER psql $DBNAME -c "delete from ir_mail_server"
+#sudo -u $TARGET_USER psql $DBNAME -c "update fetchmail_server set active=False"
+#sudo -u $TARGET_USER psql $DBNAME -c "update ir_cron set active=False"
+#sudo -u $TARGET_USER psql $DBNAME -c "update ir_config_parameter set value='http://192.168.11.233' where key='web.base.url'"
+#sudo -u $TARGET_USER psql $DBNAME -c "update ir_module_module set state='uninstalled' where name in ('dead_mans_switch_client', 'dead_mans_switch_therp')"
+#sudo -u $TARGET_USER psql $DBNAME -c "update res_users set password_crypt=(select password_crypt from res_users where login='admin')"
 
